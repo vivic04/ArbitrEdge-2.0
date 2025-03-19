57e3322675e8b4c5850eebcf2455a500b2cf9ac8
vivic04 <vihaanchugh2811@gmail.com>
2025-03-18 23:52:48 -0400

    established API connection

--- bot_architecture/test.py
+++ bot_architecture/test.py
@@ -1,8 +1,16 @@
-import ibapi
+from ibapi.wrapper import EWrapper
+from ibapi.client import EClient
 
-class IBApi(Ewrapper, EClient):
+class IBApi(EWrapper, EClient):
     def __init__(self):
         EClient.__init__(self, self)
-        
-ckass Bot()
-    ib
+       
+
+class Bot:
+    def __init__(self):
+        self.ib = IBApi()
+        self.ib.connect("127.0.0.1", 7496, 1)
+        self.ib.run()
+
+
+bot = Bot()
